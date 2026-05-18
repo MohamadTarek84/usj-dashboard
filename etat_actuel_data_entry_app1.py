7fc7111f6296e537cd61ff481921893b92d67665
MohamadTarek84 <mohamad.tarek.elhalabi@gmail.com>
2026-05-18 21:34:31 +0300

    Update etat_actuel_data_entry_app1.py

--- etat_actuel_data_entry_app1.py
+++ etat_actuel_data_entry_app1.py
@@ -1184,7 +1184,7 @@ def render_pour_finir():
     ]
 
     for i, phrase in enumerate(phrases, start=1):
-        col_label, col_boxes = st.columns([2.3, 2.7], gap="small")
+        col_label, col_boxes = st.columns([2.7, 2.3], gap="small")
 
         with col_label:
             html_block(f"""
