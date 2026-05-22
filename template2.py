c4a76895dd3774faf384d3063f77c777e642fbd2
MohamadTarek84 <mohamad.tarek.elhalabi@gmail.com>
2026-05-22 13:36:01 +0300

    Update template2.py

--- template2.py
+++ template2.py
@@ -1797,8 +1797,6 @@ def main():
             st.divider()
 
             section_header("II- OPPORTUNITES ET MENACES – NIVEAU USJ")
-            render_external_swot_intro()
-            external_swot_analysis = render_external_swot_analysis()
             quick_save_after_external_swot = render_quick_save_button("quick_save_after_external_swot")
 
             st.divider()
